249488496bb16e0b5115658c12f72d3bd8af631b
Alejandro <amax-2019189@kinal.edu.gt>
2024-03-05 22:40:58 -0600

    Se creo el login

--- Proyecto_Bimestral/config/server.js
+++ Proyecto_Bimestral/config/server.js
@@ -6,12 +6,14 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js';
 import userRoutes from '../src/users/user.routes.js';
+import authRoutes from '../src/auth/auth.routes.js';
 
 class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
         this.userPath = '/bimonthlyProyectApi/v1/users'
+        this.authPath = '/bimonthlyProyectApi/v1/auth'
 
         this.middleware();
         this.conectarDB();
@@ -32,6 +34,7 @@ class Server{
 
     routes(){
         this.app.use(this.userPath, userRoutes);
+        this.app.use(this.authPath, authRoutes);
     }
 
     listen(){
